1cfc258d6c0031c829362eafa611ee956b678ff5
Samuel Alles Remlinger <samu_65@hotmail.com>
2021-04-26 07:15:06 -0300

    ajustes

--- src/main/java/com/guairaca/tec/crudprodutos/SecurityConfig.java
+++ src/main/java/com/guairaca/tec/crudprodutos/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.guairaca.tec.crudprodutos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private CorsFilter corsFilter;
+	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.addFilterBefore(corsFilter, ChannelProcessingFilter.class);
+		
 		httpSecurity.csrf().disable().sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS) 
 		.and().authorizeRequests()
